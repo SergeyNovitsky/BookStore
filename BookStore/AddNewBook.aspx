9967d2db9cecfa9d3a6fc1eba285f0d604239156
Sergey Novitsky <novitckij.s@gmail.com>
2021-07-14 07:54:07 +0300

    Add first page for insert new book

---
+++ BookStore/AddNewBook.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddNewBook.aspx.cs" Inherits="BookStore.AddNewBook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Add new book:</div>
+        <p>            
+            <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="BookNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="SaveNewBookButton" runat="server" Text="Save" OnClick="SaveNewBookButton_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ BookStore/AddNewBook.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookStore
+{
+    public partial class AddNewBook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveNewBookButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost; Database=BookStore; Trusted_Connection=True;";
+
+            using(var connection = new SqlConnection(connectionString))
+            {
+                string query = "insert into Books (Id, Name) values (2, '" + BookNameTextBox.Text + "')";
+                SqlCommand sc = new SqlCommand(query, connection);
+                connection.Open();
+                int status = sc.ExecuteNonQuery();                
+            }
+        }
+    }
+}
