c92c245210866b18f9616837f0ddbc71557e3cca
Sergey Novitsky <novitckij.s@gmail.com>
2021-07-18 23:12:27 +0300

    Complete edit book page

--- BookStore/EditBook.aspx
+++ BookStore/EditBook.aspx
@@ -8,6 +8,55 @@
     <script src="Scripts/jquery-3.6.0.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
+            $.ajax({
+                url: 'EditBook.aspx/GetAuthors',
+                method: 'post',
+                contentType: 'application/json',
+                data: '',
+                dataType: 'json',
+                success: function (data) {
+                    $(data.d).each(function (index, author) {                        
+                        $('#selectAuthors').append('<option value="' + author.Id + '">' + author.Name + '</option>');
+                    });
+                },
+                error: function (error) {
+                    alert(error);
+                }
+            });
+            $.ajax({
+                url: 'EditBook.aspx/GetPublishers',
+                method: 'post',
+                contentType: 'application/json',
+                data: '',
+                dataType: 'json',
+                success: function (data) {
+                    $(data.d).each(function (index, publisher) {
+                        $('#selectPublishers').append('<option value="' + publisher.Id + '">' + publisher.Name + '</option>');
+                    });
+                },
+                error: function (error) {
+                    alert(error);
+                }
+            });
+            $.ajax({
+                url: 'EditBook.aspx/GetGenres',
+                method: 'post',
+                contentType: 'application/json',
+                data: '',
+                dataType: 'json',
+                success: function (data) {
+                    $(data.d).each(function (index, genre) {
+                        $('#selectGenres').append('<option value="' + genre.Id + '">' + genre.Name + '</option>');
+                    });
+                },
+                error: function (error) {
+                    alert(error);
+                }
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function () {
             $('#btnGetBook').click(function () {
                 let bookId = $('#txtId').val();
 
@@ -20,10 +69,39 @@
                     success: function (data)
                     {
                         $('#txtName').val(data.d.Name);
-                        $('#txtAuthor').val(data.d.Author);
+                        $('#selectAuthors option[value=' + data.d.Author.Id + ']').prop('selected', true);
+                        $('#selectPublishers option[value=' + data.d.Publisher.Id + ']').prop('selected', true);
+                        $('#txtPublicationYear').val(data.d.PublicationYear);
+                        $('#selectGenres option[value=' + data.d.Genre.Id + ']').prop('selected', true);
                     },
                     error: function (error)
                     {
+                        alert(error);
+                    }
+                });
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#btnSaveChanges').click(function () {
+                $.ajax({
+                    url: 'EditBook.aspx/SaveChangesAboutBook',
+                    method: 'post',
+                    contentType: 'application/json',
+                    data: JSON.stringify({
+                        bookId: $('#txtId').val(),
+                        bookName: $('#txtName').val(),
+                        authorId: $('#selectAuthors').val(),
+                        publisherId: $('#selectPublishers').val(),
+                        publicationYear: $('#txtPublicationYear').val(),
+                        genreId: $('#selectGenres').val()
+                    }),
+                    dataType: 'json',
+                    success: function (data) {
+                        alert(data.d);
+                    },
+                    error: function (error) {
                         alert(error);
                     }
                 });
@@ -35,7 +113,7 @@
     <form id="form1" runat="server">
         Book Id: 
         <input id="txtId" type="text" />
-        <input type="button" id="btnGetBook" value="Get Book" />
+        <input id="btnGetBook" type="button" value="Get Book" />
         <table>
             <tr>
                 <td>Name:</td>
@@ -43,9 +121,22 @@
             </tr>
             <tr>
                 <td>Author:</td>
-                <td><input id="txtAuthor" type="text" /></td>
+                <td><select name="Authors" id="selectAuthors"></select></td>
+            </tr>
+            <tr>
+                <td>Publisher:</td>
+                <td><select name="Publishers" id="selectPublishers"></select></td>
+            </tr>
+            <tr>
+                <td>Publication year:</td>
+                <td><input id="txtPublicationYear" type="text" /></td>
+            </tr>
+            <tr>
+                <td>Genre:</td>
+                <td><select name="Genre" id="selectGenres"></select></td>
             </tr>
         </table>
+        <input id="btnSaveChanges" type ="button" value="Save changes" />
     </form>
 </body>
 </html>
--- BookStore/EditBook.aspx.cs
+++ BookStore/EditBook.aspx.cs
@@ -9,35 +9,17 @@ namespace BookStore
 {
     public partial class EditBook : System.Web.UI.Page
     {
-        private readonly List<Author> authors = new List<Author>();
-        private readonly List<Publisher> publishers = new List<Publisher>();
-        private readonly List<Genre> genres = new List<Genre>();
         private static readonly string connectionString = ConfigurationManager.ConnectionStrings["BookStoreConnectionString"].ConnectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand("SelectAuthors", connection);
-                command.CommandType = CommandType.StoredProcedure;                
-                connection.Open();
-                SqlDataReader sqlDataReader = command.ExecuteReader();
-                while (sqlDataReader.Read())
-                {
-                    var author = new Author();
-                    author.Id = Convert.ToInt32(sqlDataReader["Id"]);
-                    author.Name = sqlDataReader["Name"].ToString();
-                    authors.Add(author);
-                }
-                connection.Close();
-            }
+            
         }
 
         [System.Web.Services.WebMethod]
         public static Book GetBookById(int bookId)
         {
-            Book book = new Book();
-            //string connectionString = ConfigurationManager.ConnectionStrings["BookStoreConnectionString"].ConnectionString;
+            Book book = new Book();            
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 SqlCommand command = new SqlCommand("GetBook", connection);
@@ -49,11 +31,102 @@ namespace BookStore
                 {
                     book.Id = Convert.ToInt32(sqlDataReader["Id"]);
                     book.Name = sqlDataReader["Name"].ToString();
-                    //book.Author = new Author { Id = }
+                    book.Author = new Author { Id = Convert.ToInt32(sqlDataReader["AuthorId"]), Name = sqlDataReader["Author"].ToString() };
+                    book.Publisher = new Publisher { Id = Convert.ToInt32(sqlDataReader["PublisherId"]), Name = sqlDataReader["Publisher"].ToString() };
+                    book.PublicationYear = Convert.ToInt32(sqlDataReader["PublicationYear"]);
+                    book.Genre = new Genre { Id = Convert.ToInt32(sqlDataReader["GenreId"]), Name = sqlDataReader["Genre"].ToString() };
                 }
             }
 
             return book;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static bool SaveChangesAboutBook(int bookId, string bookName, int authorId, int publisherId, int publicationYear, int genreId)
+        {
+            bool completedSuccessfully = false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("UpdateBook", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Id", bookId);
+                command.Parameters.AddWithValue("@Name", bookName);
+                command.Parameters.AddWithValue("@AuthorId", authorId);
+                command.Parameters.AddWithValue("@PublisherId", publisherId);
+                command.Parameters.AddWithValue("@PublicationYear", publicationYear);
+                command.Parameters.AddWithValue("@GenreId", genreId);
+                connection.Open();
+                var result = command.ExecuteNonQuery();
+                if (result == 1) completedSuccessfully = true;                
+            }
+
+            return completedSuccessfully;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static List<Author> GetAuthors()
+        {
+            List<Author> authors = new List<Author>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SelectAuthors", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    var author = new Author();
+                    author.Id = Convert.ToInt32(sqlDataReader["Id"]);
+                    author.Name = sqlDataReader["Name"].ToString();
+                    authors.Add(author);
+                }
+                connection.Close();
+            }
+            return authors;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static List<Publisher> GetPublishers()
+        {
+            List<Publisher> publishers = new List<Publisher>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SelectPublishers", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    var publisher = new Publisher();
+                    publisher.Id = Convert.ToInt32(sqlDataReader["Id"]);
+                    publisher.Name = sqlDataReader["Name"].ToString();
+                    publishers.Add(publisher);
+                }
+                connection.Close();
+            }
+            return publishers;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static List<Genre> GetGenres()
+        {
+            List<Genre> genres = new List<Genre>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SelectGenres", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    var genre = new Genre();
+                    genre.Id = Convert.ToInt32(sqlDataReader["Id"]);
+                    genre.Name = sqlDataReader["Name"].ToString();
+                    genres.Add(genre);
+                }
+                connection.Close();
+            }
+            return genres;
         }
     }
 }
